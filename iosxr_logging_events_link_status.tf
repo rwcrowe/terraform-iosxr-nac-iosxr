078e4e4e22d3a13209ea9803f6d5044a743468ef
rocrowe <rocrowe@cisco.com>
2026-08-19 10:45:25 -0400

    refactor(logging-events-link-status): update data path to logging.events.link_status
    Co-authored-by: Robert Crowe <rocrowe@cisco.com>

--- iosxr_logging_events_link_status.tf
+++ iosxr_logging_events_link_status.tf
@@ -1,6 +1,6 @@
 resource "iosxr_logging_events_link_status" "logging_events_link_status" {
-  for_each            = { for device in local.devices : device.name => device if try(local.device_config[device.name].logging.link_status, null) != null || try(local.defaults.iosxr.devices.configuration.logging.link_status, null) != null }
+  for_each            = { for device in local.devices : device.name => device if try(local.device_config[device.name].logging.events.link_status, null) != null || try(local.defaults.iosxr.devices.configuration.logging.events.link_status, null) != null }
   device              = each.value.name
-  software_interfaces = try(local.device_config[each.value.name].logging.link_status, local.defaults.iosxr.devices.configuration.logging.link_status, null) == "software-interfaces" ? true : null
-  disable             = try(local.device_config[each.value.name].logging.link_status, local.defaults.iosxr.devices.configuration.logging.link_status, null) == "disable" ? true : null
+  software_interfaces = try(local.device_config[each.value.name].logging.events.link_status, local.defaults.iosxr.devices.configuration.logging.events.link_status, null) == "software-interfaces" ? true : null
+  disable             = try(local.device_config[each.value.name].logging.events.link_status, local.defaults.iosxr.devices.configuration.logging.events.link_status, null) == "disable" ? true : null
 }
