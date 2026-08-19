12b9407cc490c32d8c3954ce747d7e3c5c713b6b
rocrowe <rocrowe@cisco.com>
2026-08-18 20:41:27 -0400

    feat(logging-link-status): add logging link status Terraform module and main.tf wiring
    Co-authored-by: Robert Crowe <rocrowe@cisco.com>

---
+++ iosxr_logging_link_status.tf
@@ -0,0 +1,6 @@
+resource "iosxr_logging_link_status" "logging_link_status" {
+  for_each            = { for device in local.devices : device.name => device if try(local.device_config[device.name].logging.link_status, null) != null || try(local.defaults.iosxr.devices.configuration.logging.link_status, null) != null }
+  device              = each.value.name
+  software_interfaces = try(local.device_config[each.value.name].logging.link_status, local.defaults.iosxr.devices.configuration.logging.link_status, null) == "software-interfaces" ? true : null
+  disable             = try(local.device_config[each.value.name].logging.link_status, local.defaults.iosxr.devices.configuration.logging.link_status, null) == "disable" ? true : null
+}
